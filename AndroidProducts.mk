90f2d456a825cc4f030b2e27c0497f3a95fb3c07
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-04-24 15:38:06 +0200

    TECNO-KJ6: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: tecno Model: TECNO KJ6 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TECNO-KJ6.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TECNO-KJ6-user \
+    omni_TECNO-KJ6-userdebug \
+    omni_TECNO-KJ6-eng
